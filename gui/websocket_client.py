6db2393bbaa3ba5d41583dd722c55744039e1f25
Carlos Efrain Rosas Medina <carlos.rosas7476@alumnos.udg.mx>
2026-05-18 11:39:39 -0600

    fix: corregir defaults del WebSocket a URL de producción

--- gui/websocket_client.py
+++ gui/websocket_client.py
@@ -36,7 +36,7 @@ class WebSocketClient(QThread):
 
     connection_status = pyqtSignal(bool)  # True=conectado, False=desconectado
     
-    def __init__(self, server_url: str = "localhost:8000"):
+    def __init__(self, server_url: str = "web-production-96c8.up.railway.app"):
         super().__init__()
         self.server_url = server_url
         self.ws = None
@@ -155,7 +155,7 @@ class WebSocketClient(QThread):
 # Instancia global (se inicializa en main)
 ws_client = None
 
-def init_websocket(server_url: str = "localhost:8000"):
+def init_websocket(server_url: str = "web-production-96c8.up.railway.app"):
     """Inicializar WebSocket global"""
     global ws_client
     if ws_client is None:
